9fe9e8e6ae210be817b0fc7f74e7ad9325bb8309
kenjimorita <morita_kenji_gn@cyberagent.co.jp>
2016-10-08 11:33:52 +0900

    todoApp

---
+++ todo-react-redux/src/core/firebase/config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCdpLAtruWYdo80eNJ5gV9HMdabAkpJ1Ks',
+  authDomain: 'atodeneapp.firebaseapp.com',
+  databaseURL: 'https://atodeneapp.firebaseio.com',
+  messagingSenderId: '700665790453'
+};
+
